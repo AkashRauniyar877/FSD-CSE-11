9d8ebd8ab102a32e94d4ac5fb88cb9cecc0be3c9
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-07-31 09:26:22 +0530

    ..

---
+++ demo1.js
@@ -0,0 +1,16 @@
+const student={
+    name:"Akash",
+    age: 20,
+    branch:"CSE"
+}
+
+// const name=student.name;
+// const age=student.age;
+// const branch=student.branch;
+
+// Destructuring
+const{name,age,branch}=student;
+console.log("Name=",name);
+console.log("Age=",age);
+console.log("Branch=",branch);
+
